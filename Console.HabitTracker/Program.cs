d450e0228e0f60b63ac4b89e147e28cc87d07f7b
JMWeeks <git@jmweeks.com>
2024-08-12 10:40:14 -0400

    Start menu structure.

--- Console.HabitTracker/Program.cs
+++ Console.HabitTracker/Program.cs
@@ -18,25 +18,76 @@
 // Contact information:
 // Email: license@jmweeks.com
 
-using SimpleMenu;
-
-Menu mainMenu = new("Habit Tracker (Console)");
-
-mainMenu.AddMenuOption(new Menu.Option("Test Menu","1"));
-mainMenu.AddMenuOption(new Menu.Option("Test Menu 2","2"));
-mainMenu.AddMenuOption(new Menu.Option("Test Menu 3","3"));
-mainMenu.AddMenuOption(new Menu.Option("Lorem ipsum","3"));
-mainMenu.AddMenuOption(new Menu.Option("Exit","0"));
-
-while (true)
+namespace Console.HabitTracker
 {
-    mainMenu.ShowMenu();
-    var s = mainMenu.Prompt(checkEnabled:true);
+    using SimpleMenu;
 
-    switch (s)
+    internal class Program
     {
-        case "0":
-            Environment.Exit(0);
-            break;
+        private static void Main(string[] args)
+        {
+            var AppName = "Habit Tracker (Console)";
+            Menu MenuMain = new($"{AppName}");
+            Menu MenuLogHabit = new($"{AppName} :: Log Habit");
+            Menu MenuViewLog = new($"{AppName} :: View Log");
+            Menu MenuReports = new($"{AppName} :: Reports");
+            Menu MenuNewHabit = new($"{AppName} :: New Habit");
+            Menu MenuSettings = new($"{AppName} :: Settings");
+
+            MenuMain.AddMenuOption(new Menu.Option("Log Habit", "1"));
+            MenuMain.AddMenuOption(new Menu.Option("View Log", "2"));
+            MenuMain.AddMenuOption(new Menu.Option("Reports", "3"));
+            MenuMain.AddMenuOption(new Menu.Option("New Habit", "4"));
+            MenuMain.AddMenuOption(new Menu.Option("Settings", "9"));
+            MenuMain.AddMenuOption(new Menu.Option("Exit", "0"));
+            
+            MenuLogHabit.AddMenuOption(new Menu.Option("Lorem Ipsum Dolor Sit Amet", "1"));
+            MenuLogHabit.AddMenuOption(new Menu.Option("Exit to Main Menu", "0"));
+            
+            MenuViewLog.AddMenuOption(new Menu.Option("Lorem Ipsum Dolor Sit Amet", "1"));
+            MenuViewLog.AddMenuOption(new Menu.Option("Exit to Main Menu", "0"));
+            
+            MenuReports.AddMenuOption(new Menu.Option("Lorem Ipsum Dolor Sit Amet", "1"));
+            MenuReports.AddMenuOption(new Menu.Option("Exit to Main Menu", "0"));
+            
+            MenuNewHabit.AddMenuOption(new Menu.Option("Lorem Ipsum Dolor Sit Amet", "1"));
+            MenuNewHabit.AddMenuOption(new Menu.Option("Exit to Main Menu", "0"));
+            
+            MenuSettings.AddMenuOption(new Menu.Option("Lorem Ipsum Dolor Sit Amet", "1"));
+            MenuSettings.AddMenuOption(new Menu.Option("Exit to Main Menu", "0"));
+
+            while (true)
+            {
+                MenuMain.ShowMenu();
+                var s = MenuMain.Prompt(checkEnabled: true);
+
+                switch (s)
+                {
+                    case "1":
+                        MenuLogHabit.ShowMenu();
+                        MenuLogHabit.Prompt(checkEnabled: true);
+                        break;
+                    case "2":
+                        MenuViewLog.ShowMenu();
+                        MenuViewLog.Prompt(checkEnabled: true);
+                        break;
+                    case "3":
+                        MenuReports.ShowMenu();
+                        MenuReports.Prompt(checkEnabled: true);
+                        break;
+                    case "4":
+                        MenuNewHabit.ShowMenu();
+                        MenuNewHabit.Prompt(checkEnabled: true);
+                        break;
+                    case "9":
+                        MenuSettings.ShowMenu();
+                        MenuSettings.Prompt(checkEnabled: true);
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
     }
 }
