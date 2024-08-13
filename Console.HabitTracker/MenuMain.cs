ccfbc89711c11cc8be7f1dc7b748fca1122f8ae1
JMWeeks <git@jmweeks.com>
2024-08-13 02:42:48 -0400

    Rename Menus.

--- Console.HabitTracker/MenuMain.cs
+++ Console.HabitTracker/MenuMain.cs
@@ -6,8 +6,8 @@ namespace Console.HabitTracker{
         public MenuMain(){
             AddMenuOption(new Option("Log Habit", "1"));
             AddMenuOption(new Option("View Log", "2"));
-            AddMenuOption(new Option("Reports", "3"));
-            AddMenuOption(new Option("New Habit", "4"));
+            AddMenuOption(new Option("Report Menu", "3"));
+            AddMenuOption(new Option("Add New Habit", "4"));
             AddMenuOption(new Option("Settings", "9"));
             AddMenuOption(new Option("Exit", "0"));
             ShowMenu();
