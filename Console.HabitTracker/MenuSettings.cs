ee646f9e580c181b341ce5f406a41f9b37a7086f
JMWeeks <git@jmweeks.com>
2024-08-13 02:43:29 -0400

    Add database configuration option to Menu.

--- Console.HabitTracker/MenuSettings.cs
+++ Console.HabitTracker/MenuSettings.cs
@@ -9,6 +9,7 @@ namespace Console.HabitTracker{
             AddMenuOption(new MenuSettings.Option("Add Demo Data to Database", "3"));
             AddMenuOption(new MenuSettings.Option("Backup Database", "4"));
             AddMenuOption(new MenuSettings.Option("Export Database", "5"));
+            AddMenuOption(new MenuSettings.Option("Database Config", "6"));
             AddMenuOption(new MenuSettings.Option("Exit to Main Menu", "0"));
         }
 
