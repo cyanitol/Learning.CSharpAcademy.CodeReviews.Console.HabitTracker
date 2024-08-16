936b3f43dbaf4ac6f6fd7776073154850ccb0209
JMWeeks <git@jmweeks.com>
2024-08-16 18:38:53 -0400

    Remove initial menu from constructor.

--- Console.HabitTracker/MenuMain.cs
+++ Console.HabitTracker/MenuMain.cs
@@ -1,17 +1,17 @@
 using SimpleMenu;
 
-namespace Console.HabitTracker{
-    internal class MenuMain : Menu {
-
+namespace Console.HabitTracker
+{
+    internal class MenuMain : Menu
+    {
         public MenuMain(string title = "Application Title") : base(title)
         {
-            AddMenuOption(new Option("Log Habit", "1"));
-            AddMenuOption(new Option("View Log", "2"));
-            AddMenuOption(new Option("Report Menu", "3"));
-            AddMenuOption(new Option("Add New Habit", "4"));
-            AddMenuOption(new Option("Settings", "9"));
-            AddMenuOption(new Option("Exit", "0"));
-            ShowMenu();
+                AddMenuOption(new Option("Log Habit", "1"));
+                AddMenuOption(new Option("View Log", "2"));
+                AddMenuOption(new Option("Report Menu", "3"));
+                AddMenuOption(new Option("Add New Habit", "4"));
+                AddMenuOption(new Option("Settings", "9"));
+                AddMenuOption(new Option("Exit", "0"));
         }
     }
 }
