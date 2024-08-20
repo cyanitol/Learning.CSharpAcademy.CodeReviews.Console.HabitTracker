25e8a8353b3d4198b790cd310dd3946fbd7a9af4
JMWeeks <git@jmweeks.com>
2024-08-19 22:34:02 -0400

    Refactor, comments, and fix issue with menu centering.

--- SimpleMenu/Menu.cs
+++ SimpleMenu/Menu.cs
@@ -33,7 +33,7 @@ public class Menu(string title = "Application Title")
     private List<Option> _options = [];
 
     private static readonly int ConsoleWidth = Console.WindowWidth;
-    private readonly int _totalPaddingLength = ConsoleWidth / 2 + title.Length / 2;
+    private readonly int _totalPaddingLength = (ConsoleWidth + title.Length) / 2;
     private const string MenuBorder = "|";
     private const int InsideMarginWidth = 2;
     private const int OutsideMarginWidth = 2;
@@ -53,22 +53,34 @@ public class Menu(string title = "Application Title")
             _options.Remove(option);
         }
     }
-    public void ShowMenu(bool clear = true, string optionDelimiter = "",
-        List<string>? footerContent = null)
+    public void ShowMenu(bool clear = true, string optionDelimiter = "", List<string>? footerContent = null)
     {
         if (clear)
             Console.Clear();
 
         var menuTitle = new StringBuilder();
+
+        // Calculate left padding for centering the title
+        int titleLength = title.Length;
+        int leftPadding = (ConsoleWidth - titleLength) / 2;
+
+        // Top border
         menuTitle.AppendFormat($"{_outsideMargin}");
-        menuTitle.AppendFormat("".PadLeft(_totalPaddingLength - OutsideMarginWidth, '='));
+        menuTitle.AppendFormat("".PadLeft(ConsoleWidth - OutsideMarginWidth - 2, '='));  // Subtract 2 to reduce the number of '='
         menuTitle.AppendFormat("\n");
+
+        // Title line
         menuTitle.AppendFormat($"{_outsideMargin}");
-        menuTitle.AppendFormat($"{title}".PadLeft(ConsoleWidth - menuTitle.Length).ToUpper());
+        menuTitle.AppendFormat("".PadLeft(leftPadding, ' '));
+        menuTitle.AppendFormat($"{title.ToUpper()}");
+        menuTitle.AppendFormat("".PadRight(ConsoleWidth - leftPadding - titleLength - OutsideMarginWidth, ' '));
         menuTitle.AppendFormat("\n");
+
+        // Bottom border
         menuTitle.AppendFormat($"{_outsideMargin}");
-        menuTitle.AppendFormat("".PadLeft(_totalPaddingLength - OutsideMarginWidth, '='));
+        menuTitle.AppendFormat("".PadLeft(ConsoleWidth - OutsideMarginWidth - 2, '='));  // Subtract 2 to reduce the number of '='
         menuTitle.AppendFormat("\n");
+
         Console.Write(menuTitle);
 
         foreach (var option in _options)
@@ -78,45 +90,98 @@ public class Menu(string title = "Application Title")
             menuLine.AppendFormat(MenuBorder);
             menuLine.AppendFormat($"{_insideMargin}");
             menuLine.AppendFormat($"{option.Selector}{optionDelimiter}");
-            menuLine.AppendFormat(new string(' ', SelectorDescriptionMargin -
-                                                  option.Selector.Length - optionDelimiter.Length));
+            menuLine.AppendFormat(new string(' ', SelectorDescriptionMargin - option.Selector.Length - optionDelimiter.Length));
             menuLine.AppendFormat($"{option.Description}");
-            menuLine.AppendFormat("".PadLeft(_totalPaddingLength - menuLine.Length - 1));
+
+            // Calculate right padding to align the ending '|'
+            int optionLineLength = menuLine.Length;
+            int rightPadding = ConsoleWidth - optionLineLength - OutsideMarginWidth - 1;
+
+            menuLine.AppendFormat("".PadRight(rightPadding));
             menuLine.AppendFormat(MenuBorder);
             menuLine.AppendFormat($"{_outsideMargin}");
             menuLine.AppendFormat("\n");
-            Console.Write(menuLine);
 
+            Console.Write(menuLine);
         }
-        ShowFooter(footerContent ?? []);
+        ShowFooter(footerContent ?? new List<string>());
     }
 
-    private void ShowFooter(string content = "")
+    private void ShowFooter(string content = "", bool centered = false)
     {
         var footer = new StringBuilder();
+        int footerPaddingLength = ConsoleWidth - (OutsideMarginWidth * 2);
+
+        // Top footer border
         footer.AppendFormat($"{_outsideMargin}");
-        footer.AppendFormat("".PadLeft(_totalPaddingLength - OutsideMarginWidth, '-'));
+        footer.AppendFormat("".PadLeft(footerPaddingLength, '-'));
+        footer.AppendFormat($"{_outsideMargin}\n");
+
+        // Content line, left-justified or centered based on the flag
+        if (!string.IsNullOrEmpty(content))
+        {
+            if (centered)
+            {
+                int contentPadding = (footerPaddingLength - content.Length) / 2;
+                footer.AppendFormat($"{_outsideMargin}");
+                footer.AppendFormat("".PadLeft(contentPadding, ' '));
+                footer.AppendFormat(content);
+                footer.AppendFormat("".PadRight(footerPaddingLength - contentPadding - content.Length, ' '));
+                footer.AppendFormat($"{_outsideMargin}\n");
+            }
+            else
+            {
+                footer.AppendFormat($"{_outsideMargin}");
+                footer.AppendFormat(content);
+                footer.AppendFormat("".PadRight(footerPaddingLength - content.Length, ' '));
+                footer.AppendFormat($"{_outsideMargin}\n");
+            }
+        }
+
+        // Bottom footer border
         footer.AppendFormat($"{_outsideMargin}");
-        footer.AppendFormat("\n");
-        footer.AppendFormat($"{_outsideMargin}{_insideMargin}{content}{_outsideMargin}\n");
+        footer.AppendFormat("".PadRight(footerPaddingLength, '-'));
         footer.AppendFormat($"{_outsideMargin}");
-        footer.AppendFormat("".PadRight(_totalPaddingLength - OutsideMarginWidth, '-'));
+
         Console.WriteLine(footer.ToString());
     }
 
-    private void ShowFooter(List<string> content)
+    private void ShowFooter(List<string> content, bool centered = false)
     {
         var footer = new StringBuilder();
+        int footerPaddingLength = ConsoleWidth - (OutsideMarginWidth * 2);
+
+        // Top footer border
         footer.AppendFormat($"{_outsideMargin}");
-        footer.AppendFormat("".PadLeft(_totalPaddingLength - OutsideMarginWidth, '-'));
-        footer.AppendFormat($"{_outsideMargin}");
-        footer.AppendFormat("\n");
+        footer.AppendFormat("".PadLeft(footerPaddingLength, '-'));
+        footer.AppendFormat($"{_outsideMargin}\n");
+
+        // Multiple content lines, left-justified or centered based on the flag
         foreach (var item in content)
         {
-            footer.AppendFormat($"{_outsideMargin}{_insideMargin}{item}{_outsideMargin}\n");
+            if (centered)
+            {
+                int contentPadding = (footerPaddingLength - item.Length) / 2;
+                footer.AppendFormat($"{_outsideMargin}");
+                footer.AppendFormat("".PadLeft(contentPadding, ' '));
+                footer.AppendFormat(item);
+                footer.AppendFormat("".PadRight(footerPaddingLength - contentPadding - item.Length, ' '));
+                footer.AppendFormat($"{_outsideMargin}\n");
+            }
+            else
+            {
+                footer.AppendFormat($"{_outsideMargin}");
+                footer.AppendFormat(item);
+                footer.AppendFormat("".PadRight(footerPaddingLength - item.Length, ' '));
+                footer.AppendFormat($"{_outsideMargin}\n");
+            }
         }
+
+        // Bottom footer border
         footer.AppendFormat($"{_outsideMargin}");
-        footer.AppendFormat("".PadRight(_totalPaddingLength - OutsideMarginWidth, '-'));
+        footer.AppendFormat("".PadRight(footerPaddingLength, '-'));
+        footer.AppendFormat($"{_outsideMargin}");
+
         Console.WriteLine(footer.ToString());
     }
 
