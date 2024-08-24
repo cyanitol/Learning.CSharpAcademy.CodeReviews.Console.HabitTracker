6aaf7fc2e8126646b53790805447d3198c578c3c
JMWeeks <git@jmweeks.com>
2024-08-24 15:13:25 -0400

    Update settings menu options

--- Console.HabitTracker/MenuSettings.cs
+++ Console.HabitTracker/MenuSettings.cs
@@ -8,11 +8,8 @@ internal class MenuSettings : Menu
     public MenuSettings(string title = "Application Title") : base(title)
     {
         AddMenuOption(new Option("Modify Habit", "1"));
-        AddMenuOption(new Option("Clear Database", "2"));
-        AddMenuOption(new Option("Add Demo Data to Database", "3"));
-        AddMenuOption(new Option("Backup Database", "4"));
-        AddMenuOption(new Option("Export Database", "5"));
-        AddMenuOption(new Option("Database Config", "6"));
+        AddMenuOption(new Option("Delete Habit", "2"));
+        AddMenuOption(new Option("Reset Database with Demo Data", "3"));
         AddMenuOption(new Option("Exit to Main Menu", "0"));
         ShowMenu();
         Program.MenuSelection = Prompt(checkEnabled: true);
